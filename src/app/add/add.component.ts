8c7b0d0edf5093bbe49cb6c3e959734382e5f2b5
Akash Pandey <33104094+akashpandey1310@users.noreply.github.com>
2021-12-29 18:01:09 +0530

    initial commit

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -13,6 +13,12 @@ export class AddComponent implements OnInit {
 
   AddressesArray!: FormArray ;
 
+  addressArray: any;
+
+  address_maxLength = 10;
+
+  submitForm: Object = {};
+
   constructor(private testService:TestServiceService,private _formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
@@ -21,18 +27,53 @@ export class AddComponent implements OnInit {
       AddressesArray : this._formBuilder.array([ this.createAddressRow() ])
     })
 
+    this.addUserForm = this._formBuilder.group({
+     id: [''],
+     fname: ['', Validators.required],
+     lname: ['', Validators.required],
+    })
+
   }
 
-  onAddNew_VV(): void{
+  onAddNew_AddressRow(): void{
     this.AddressesArray = this.addressForm.get('AddressesArray') as FormArray;
     this.AddressesArray.push(this.createAddressRow());
   }
 
   createAddressRow(): FormGroup {
     return this._formBuilder.group({
-      viral_video_1_title: [''],
-      viral_video_1_URL: ['']
+      street_1: [''],
+      country_1: [''],
+      zip_1: ['']
     });
+  }
+
+  onSubmit(){
+    console.log(this.addUserForm.value);
+
+
+    this.addressArray = this.addressForm.value.AddressesArray;
+
+    for(var index=0; index< this.address_maxLength ; index++) {
+
+      var AddresForm = {
+        [ `street_${index+1}`] : this.addressArray[index]? this.addressArray[index].street_1: '',
+        [ `country_${index+1}`] : this.addressArray[index]? this.addressArray[index].country_1: '',
+        [ `zip_${index+1}`] : this.addressArray[index]? this.addressArray[index].zip_1: '',
+      }
+
+      Object.assign(this.submitForm, AddresForm)
+    }
+
+    var user_form = {
+      id: this.addUserForm.value.id,
+      fname: this.addUserForm.value.fname,
+      lname: this.addUserForm.value.lname,
+    }
+
+    Object.assign(this.submitForm, user_form)
+
+
   }
 
 }
