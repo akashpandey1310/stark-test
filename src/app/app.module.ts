82825698b1e5b81fbb8a59e22c77a4ed38156c6c
Akash Pandey <33104094+akashpandey1310@users.noreply.github.com>
2021-12-29 18:03:10 +0530

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DetailsComponent } from './details/details.component'
 import { BearerService } from './service/bearer.service';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { BearerService } from './service/bearer.service';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [
